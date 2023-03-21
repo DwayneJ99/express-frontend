c73a888a57a5dda9b8189d3962a2dec97bf288ab
Dwayne Jones <dwaynejones13123@gmail.com>
2023-03-20 20:54:34 -0400

    frontend complete

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Route, Routes} from 'react-router-dom'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+function App() {
+
+  const URL = 'https://express-backend-lab.herokuapp.com/'
+
+
+  return (
+    <div className="App">
+      <Header/>
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/projects' element={<Projects URL={URL}/>} />
+        <Route exact path='/about' element={<About URL={URL}/>} />
+         </Routes>
+         <Footer/>
+    </div>
+  );
+}
+
+export default App;
